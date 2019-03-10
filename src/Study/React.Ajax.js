013951e94668a3b1e8da2fb2450524063174c9ea
HvaiY <337646685@qq.com>
2019-03-10 16:46:44 +0800

    Fetch 请求api 获取数据

--- src/Study/React.Ajax.js
+++ src/Study/React.Ajax.js
@@ -12,26 +12,50 @@ class UserGist extends React.Component {
   componentDidMount() {
     //接下来操作state时上下文对象发生改变，此处拿到操作句柄
     var that = this;
-    //ajax请求
-    this.serverRequest = $.ajax({
-      url: this.props.source,
-      type: "GET",
-      dataType: "jsonp",
-      success: function(result) {
-        console.log(result.data);
-        var lastGist = result.data[0];
-        //此处的上下文发生改变，this的指针指向发生了变化，通过上述定义的that来操作
-        that.setState({
-          username: lastGist.owner.login,
-          lastGistUrl: lastGist.html_url
+    fetch(this.props.source, {
+      method: "GET",
+      dataType: "jsonp"
+    })
+      .then(function(response) {
+        if (response.status !== 200) {
+          console.log("存在一个问题，状态码为：" + response.status);
+          return;
+        }
+        // 响应信息
+        console.log(response);
+        // 设置响应文本
+        response.json().then(function(data) {
+          console.log(data);
+          var lastGist = data[0];
+          that.setState({
+            username: lastGist.owner.login,
+            lastGistUrl: lastGist.html_url
+          });
         });
-      }
-    });
+      })
+      .catch(function(error) {
+        console.log("Fetch 错误：" + error);
+      });
+    // //ajax请求 需要引入jquery.js
+    // this.serverRequest = $.ajax({
+    //   url: this.props.source,
+    //   type: "GET",
+    //   dataType: "jsonp",
+    //   success: function(result) {
+    //     console.log(result.data);
+    //     var lastGist = result.data[0];
+    //     //此处的上下文发生改变，this的指针指向发生了变化，通过上述定义的that来操作
+    //     that.setState({
+    //       username: lastGist.owner.login,
+    //       lastGistUrl: lastGist.html_url
+    //     });
+    //   }
+    // });
   }
 
   //卸载React组件时，把多余请求关闭，以免影响其他框架或组件的操作
   componentWillUnmount() {
-    this.serverRequest.abort();
+    //  this.serverRequest.abort(); // ajax 需要释放的
   }
 
   render() {
@@ -40,6 +64,11 @@ class UserGist extends React.Component {
         {this.state.username} 用户最新的 Gist 共享地址：
         <a href={this.state.lastGistUrl} rel="nofollow">
           {this.state.lastGistUrl}
+        </a>
+        <hr />
+        <a href="http://www.cnblogs.com/JsonGoIt/p/9395058.html">
+          {" "}
+          Fetch 点击了解更多
         </a>
       </div>
     );
